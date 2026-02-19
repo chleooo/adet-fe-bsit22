8759f855b7af7e60fb79a0dded45cb5e95595896
Chloe Nicole Monteza <cnmonteza64178@liceo.edu.ph>
2026-02-18 23:47:11 -0800

    Update posts.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,27 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
+  standalone: true,
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-
-  posts = [
-    {
-      title: 'ADET',
-      description: 'Developing Blog Posts List UI in Angular.'
-    },
-    {
-      title: 'OOP',
-      description: 'Building Applicationg Development.'
-    },
-    {
-      title: 'NETWORKING',
-      description: 'Teambuilding Network Design.'
-    }
-  ];
-
-  deletePost(index: number) {
-    this.posts.splice(index, 1);
-  }
-}
+export class PostsComponent {}
